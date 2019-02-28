70931927a3ef04ce518cfb9e859334efab9bf6ee
Soyombo Soyinka <showy4show@gmail.com>
2019-02-28 21:50:56 +0100

    Deletion of commented codes

--- app/src/main/java/com/soyinka/soyombo/quizapp/MainActivity.kt
+++ app/src/main/java/com/soyinka/soyombo/quizapp/MainActivity.kt
@@ -11,30 +11,17 @@ import android.widget.Button
 import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
-//import com.soyinka.soyombo.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main )
-
-        //input to EditText is caught so as to be passed to the other activity class
-        //val nameField = findViewById<EditText>(R.id.name_field)
-        //binding.nameField.text
-        name_field.text
-        //val takeQuiz = findViewById<Button>(R.id.take_quiz)
-
         /**
          * this sets onClickListener on the button element; this is another way of setting onClick listener in java...
          * on a button instead of declaring it in xml file
          */
         take_quiz.setOnClickListener {
-            //two intents are passed at a time i.e to get to the next activity and to throw the user name from EditText with putExtra()
-            //val quizIntent = Intent(this@MainActivity, QuizActivity::class.java)
             val quizIntent = Intent(this, QuizActivity::class.java)
             quizIntent.putExtra("EdiTtEXTvALUE", "Welcome " + name_field.text)
             startActivity(quizIntent)
--- app/src/main/java/com/soyinka/soyombo/quizapp/QuizActivity.kt
+++ app/src/main/java/com/soyinka/soyombo/quizapp/QuizActivity.kt
@@ -13,63 +13,45 @@ import kotlinx.android.synthetic.main.question.*
 
 class QuizActivity : AppCompatActivity() {
 
-    //private lateinit var binding2: ActivityMainBinding
-
-    //@global: variable to keep track of the number of correct answers
     internal var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.question)
-        //binding2 = DataBindingUtil.setContentView(this, R.layout.question)
 
         /**
          * Username from MainActivity is thrown down here, so that it can be displayed in this activity class.
          * @param: username from MainActivity
          */
-        //val setUserName = findViewById<TextView>(R.id.welcome)
         welcome.text = intent.getStringExtra("EdiTtEXTvALUE")
     }
 
     fun submitAnswer(view: View) {
         //This assesses the correct answer to question 1.
-        //val questionOneAnswer = findViewById<RadioButton>(R.id.none)
-        //none
         val answerToQuestion1 = none.isChecked
         if (answerToQuestion1) {
             score += 1
         }
         //This assesses the correct answer to question 2.
-        //val questionTwoAnswer = findViewById<RadioButton>(R.id.undefined)
         val answerToQuestion2 = undefined.isChecked
         if (answerToQuestion2) {
             score += 1
         }
         //This assesses the correct answer to question 3.
-        //val questionThreeAnswer = findViewById<RadioButton>(R.id.entityB)
         val answerToQuestion3 = entityB.isChecked
         if (answerToQuestion3) {
             score += 1
         }
         //This assesses the correct answer to question 4.
-        //val questionFourAnswer = findViewById<RadioButton>(R.id.entityA1)
         val answerToQuestion4 = entityA1.isChecked
         if (answerToQuestion4) {
             score += 1
         }
         //This assesses the correct answer to question 5.
-        //val questionFiveAnswerA = findViewById<CheckBox>(R.id.abc)
         val answerToQuestion5a = abc.isChecked
-
-        //val questionFiveAnswerB = findViewById<CheckBox>(R.id.def)
         val answerToQuestion5b = def.isChecked
-
-        //val questionFiveAnswerC = findViewById<CheckBox>(R.id.bcd)
         val answerToQuestion5c = bcd.isChecked
-
-        //val questionFiveAnswerD = findViewById<CheckBox>(R.id.cde)
         val answerToQuestion5d = cde.isChecked
-
 
         score += when {
             (answerToQuestion5a && answerToQuestion5b) && !answerToQuestion5d && !answerToQuestion5c -> 1
@@ -77,15 +59,11 @@ class QuizActivity : AppCompatActivity() {
         }
 
         //This assesses the correct answer to question 6.
-        //val questionSixAnswer = findViewById<EditText>(R.id.edit)
         val answerToQuestion6 = edit.text.toString()
         score += when (answerToQuestion6) {
             "Particle A" -> 1
             else -> 0
         }
-//        if (answerToQuestion6 === "Particle A") {
-//            score += 1
-//        }
         /**
          * toast messages to display on submission and the navigation intent to restart the quiz
          */
@@ -97,5 +75,4 @@ class QuizActivity : AppCompatActivity() {
         startActivity(Intent(this@QuizActivity, MainActivity::class.java))
         finish()
     }
-
 }
